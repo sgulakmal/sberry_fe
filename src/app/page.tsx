460cf0e7634c52fbd238874df4c9ccdbbceb4e86
Lakmal Siyambalage <ulakmal@fetchseed.com>
2025-06-02 23:14:14 +0530

    login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,20 @@ import { RootState } from "@/lib/store";
 import { useDispatch, useSelector } from "react-redux";
 import Reactions from "./components/Reactions";
 import CommentComponent from "./components/Comments";
+import { useRouter } from 'next/navigation';
+import { useEffect } from "react";
 
 export default function Home() {
-
+  const router = useRouter()
   const dispatch = useDispatch();
   const user = useSelector((state: RootState) => state.auth);
+
+  useEffect(() => {
+    if (!user.isLoggedIn) {
+      router.push('/login');
+    }
+  }, [user.isLoggedIn]);
+
 
   return (
     <div>
@@ -22,13 +31,13 @@ export default function Home() {
             <h2>Post #1</h2>
             <p>Hi all</p>
             <Reactions postId="post2" />
-            <CommentComponent postId="post2"/>
+            <CommentComponent postId="post2" />
           </div>
           <div style={{ padding: '20px' }}>
             <h2>Post #1</h2>
             <p>Hi all second post</p>
             <Reactions postId="post1" />
-               <CommentComponent postId="post1"/>
+            <CommentComponent postId="post1" />
           </div>
         </>
       ) : (
